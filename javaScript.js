b5578c3c39f2a812449b6cf67416962754740716
Richard Burneo <rchrdbrneo@gmail.com>
2022-05-20 01:09:35 -0500

    Add popum model detail

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,129 @@
+const projects = [
+  {
+    name: 'Multi-Post Stories 1',
+    language: [
+      'css',
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    image: './images/MultiSotry.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    live: 'https://reflexionesrichardburneo.blogspot.com/',
+    source: 'https://github.com/pirc25',
+  },
+  {
+    name: 'Data Dashboard Healthcare 2',
+    language: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    image: './images/Img MultiSotry.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    live: 'https://reflexionesrichardburneo.blogspot.com/',
+    source: 'https://github.com/pirc25',
+  },
+  {
+    name: 'Website Portfolio 3',
+    language: [
+      'css',
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    image: './images/MultiSotry.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    live: 'https://reflexionesrichardburneo.blogspot.com/',
+    source: 'https://github.com/pirc25',
+  },
+  {
+    name: 'Profesional Art Prining Data More 4',
+    language: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    image: './images/MultiSotry.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    live: 'https://reflexionesrichardburneo.blogspot.com/',
+    source: 'https://github.com/pirc25',
+  },
+  {
+    name: 'Data Dashboard Healthcare 5',
+    language: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    image: './images/MultiSotry.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    live: 'https://reflexionesrichardburneo.blogspot.com/',
+    source: 'https://github.com/pirc25',
+  },
+  {
+    name: 'Website Portfolio 6',
+    language: [
+      'css',
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    image: './images/MultiSotry.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    live: 'https://reflexionesrichardburneo.blogspot.com/',
+    source: 'https://github.com/pirc25',
+  },
+  {
+    name: 'Profesional Art Prining Data More 7',
+    language: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    image: './images/MultiSotry.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    live: 'https://reflexionesrichardburneo.blogspot.com/',
+    source: 'https://github.com/pirc25',
+  },
+];
+
+const detail = document.getElementById('detail');
+const detailTitle = document.getElementById('detailTitle');
+const detailDescription = document.getElementById('detailDescription');
+const detailLanguajes = document.getElementById('detailLanguajes');
+const btnDetailLive = document.getElementById('btnDetailLive');
+const btnDetailSource = document.getElementById('btnDetailSource');
+const detailImage = document.getElementById('detailImage');
+
+const openDetail = (projectNumber = null) => {
+  if (projectNumber != null) {
+    const lan = projects[projectNumber].language;
+    let lanShow = '';
+
+    lan.forEach((element) => { lanShow += `<li>${element}</li>`; });
+    detail.style.width = '100%';
+    detailTitle.innerText = projects[projectNumber].name;
+    detailDescription.innerText = projects[projectNumber].description;
+    detailImage.src = projects[projectNumber].image;
+    btnDetailLive.href = projects[projectNumber].live;
+    btnDetailSource.href = projects[projectNumber].source;
+    detailLanguajes.innerHTML = lanShow;
+  }
+};
+
+const closeDetail = () => {
+  detail.style.width = '0%';
+};
+
 function openMenu() {
   document.getElementById('my-menu').style.height = '100%';
 }
-function closeMenu(name) {
+function closeMenu() {
   document.getElementById('my-menu').style.height = '0%';
 }
- 
 
+openMenu();
+closeMenu();
+openDetail();
+closeDetail();
